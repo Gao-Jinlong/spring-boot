394b7ab3dae8a64565109b0c19d32d5d6ef76c8b
gao-jinlong <2675130594@qq.com>
2023-12-25 23:05:28 +0800

    first commit

---
+++ src/main/java/com/example/SpringBootManualApplication.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class SpringBootManualApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootManualApplication.class, args);
+    }
+}
