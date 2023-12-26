a632c60086e154a94a17ca8b684f83ab91bb2e31
gao-jinlong <2675130594@qq.com>
2023-12-26 23:22:31 +0800

    feat(config)

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,12 +1,25 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+  @Value("${email.user[0]}")
+  private String user;
+
+  @Value("${email.code}")
+  private String code;
+
+  // @Value("${email.host}")
+  // private String host;
+
+  // @Value("${email.auth}")
+  // private boolean auth;
+
   @RequestMapping("/hello")
   public String hello() {
-    return "Hello ";
+    return "Hello Spring Boot!" + user + " " + code;
   }
 }
