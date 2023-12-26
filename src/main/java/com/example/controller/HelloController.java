9f80b687606b92866127d8b4f68322fc75592466
gao-jinlong <2675130594@qq.com>
2023-12-26 23:41:28 +0800

    feat(config)

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -6,13 +6,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@Component
+@Component // 用于将类标识为Bean，只有标识为Bean的类才能被Spring容器所管理，从而使得其中的属性可以注入
 @RestController
-@EnableConfigurationProperties
 @ConfigurationProperties(prefix = "email")
 public class HelloController {
   // @Value("${email.user[0]}")
@@ -36,7 +34,7 @@ public class HelloController {
     return user;
   }
 
-  public void setUser(List<String> user) {
+  public void setUser(List<String> user) { // 注意：这里的set方法必须要有，否则无法注入，因为SpringBoot是通过set方法注入的
     this.user = user;
   }
 
