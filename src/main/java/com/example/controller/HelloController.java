4bdfcdd4f4d6907f443fc1244b29ed85f521d2bc
gao-jinlong <2675130594@qq.com>
2023-12-26 23:33:03 +0800

    feat(config)

--- src/main/java/com/example/controller/HelloController.java
+++ src/main/java/com/example/controller/HelloController.java
@@ -1,25 +1,51 @@
 package com.example.controller;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Component
 @RestController
+@EnableConfigurationProperties
+@ConfigurationProperties(prefix = "email")
 public class HelloController {
-  @Value("${email.user[0]}")
-  private String user;
+  // @Value("${email.user[0]}")
+  private List<String> user;
 
-  @Value("${email.code}")
+  // @Value("${email.code}")
   private String code;
 
   // @Value("${email.host}")
-  // private String host;
+  private String host;
 
   // @Value("${email.auth}")
-  // private boolean auth;
+  private boolean auth;
 
   @RequestMapping("/hello")
   public String hello() {
     return "Hello Spring Boot!" + user + " " + code;
   }
+
+  public List<String> getUser() {
+    return user;
+  }
+
+  public void setUser(List<String> user) {
+    this.user = user;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
 }
