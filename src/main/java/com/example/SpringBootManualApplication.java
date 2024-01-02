8926290babf4a31a55376657c7128e0ae7742247
gao-jinlong <2675130594@qq.com>
2024-01-02 23:20:15 +0800

    feat(@Bean)

--- src/main/java/com/example/SpringBootManualApplication.java
+++ src/main/java/com/example/SpringBootManualApplication.java
@@ -2,12 +2,22 @@ package com.example;
 
 import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan(basePackages = "com")
 @SpringBootApplication
 public class SpringBootManualApplication {
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootManualApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootManualApplication.class, args);
+
+        String message = context.getBean(String.class);
+        System.out.println(message);
+    }
+
+    @Bean
+    public String injectToIoC() {
+        return new String("注入到 IoC 容器");
     }
 }
