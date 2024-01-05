c4d8fe38c96c62a9d5f848c12b2b17390ddb96de
gao-jinlong <2675130594@qq.com>
2024-01-05 23:24:20 +0800

    feat(autoImport)

--- src/main/java/com/example/SpringBootManualApplication.java
+++ src/main/java/com/example/SpringBootManualApplication.java
@@ -17,7 +17,12 @@ import com.example.service.impl.UserServiceImpl;
 // @Import(CommonConfig.class) // 手动注册类到 IoC
 // @Import(CommonImportSelector.class) // 使用 ImportSelector 手动注册类到 IoC
 @EnableCommonConfig // 使用自定义组合注解
-@SpringBootApplication
+@SpringBootApplication // sporingboot 的starter依赖中也是使用相同的方式完成的自动注入，具体注解为
+                       // @EnableAutoConfiguration，@SpringBootApplication
+                       // sporingboot的starter依赖中也是使用相同的方式完成的自动注入，具体注解为
+                       // @EnableAutoConfiguration，AutoConfigurationImportSelector
+                       // spring/org.springframework.boot.autoconfigure.AutoConfiguration.imports 配置文件
+
 public class SpringBootManualApplication {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(SpringBootManualApplication.class, args);
