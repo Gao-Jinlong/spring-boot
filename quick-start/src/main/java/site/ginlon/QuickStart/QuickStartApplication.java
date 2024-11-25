6172c3ad64d0929acbd9ecb5a11dd48b91dd278e
ginlon <ginlon5241@gmail.com>
2024-11-25 23:53:32 +0800

    feat: application

--- quick-start/src/main/java/site/ginlon/QuickStart/QuickStartApplication.java
+++ quick-start/src/main/java/site/ginlon/QuickStart/QuickStartApplication.java
@@ -1,5 +1,6 @@
 package site.ginlon.QuickStart;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class QuickStartApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(QuickStartApplication.class, args);
+		SpringApplication app = new SpringApplication(QuickStartApplication.class);
+		app.setBannerMode(Banner.Mode.OFF);
+		app.run(args);
 	}
 }
